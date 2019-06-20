e07bb32be5824227a4173c52cd40f87ee4a77801
Romain MARCA <romain.marca@sii.fr>
2019-06-20 11:30:01 +0200

    first and last commit

--- TestDate.java
+++ TestDate.java
@@ -2,7 +2,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
-import java.util.Scanner; 
+import java.util.Scanner;
 
 class TestDate {
 
@@ -18,6 +18,40 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
-        
+
+        Calendar calendar = new GregorianCalendar(year, month, day);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        Date date = calendar.getTime();
+        System.out.println("The date is : " + sdf.format(date));
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        String dayWeek = "";
+        switch (dayOfWeek) {
+        case 1:
+            dayWeek = "SUNDAY";
+            break;
+        case 2:
+            dayWeek = "MONDAY";
+            break;
+        case 3:
+            dayWeek = "TUESDAY";
+            break;
+        case 4:
+            dayWeek = "WEDNESDAY";
+            break;
+        case 5:
+            dayWeek = "THURSDAY";
+            break;
+        case 6:
+            dayWeek = "FRIDAY";
+            break;
+        case 7:
+            dayWeek = "SATURDAY";
+            break;
+        default:
+            dayWeek = "To day";
+
+        }
+        System.out.println("The day of the week is : " + dayOfWeek + ", And this day is : " + dayWeek);
+
     }
 }
